717bf1e86dc5266faf8acddd5fafdbf283c83d1b
greysun <czh60601@126.com>
2017-09-27 21:53:06 +0800

    在DBUtils里添加获取表数据条数方法

--- CommonMVC/src/top/greysun/common/util/DBUtils.java
+++ CommonMVC/src/top/greysun/common/util/DBUtils.java
@@ -1,5 +1,9 @@
 package top.greysun.common.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class DBUtils extends OracleUtils {
 
@@ -7,4 +11,24 @@ public class DBUtils extends OracleUtils {
 		return true;
 	}
 
+	public static int getCount(String tbName){
+		String sql = "SELECT COUNT(*) FROM "+tbName;
+		int rowCount = 0;  
+		try {  
+			Connection conn = DBUtils.getConnection();  
+			PreparedStatement psmt = conn.prepareStatement(sql);  
+			ResultSet res = psmt.executeQuery();  
+			while(res.next()){  
+				rowCount = res.getInt(1);  
+			}  
+			res.close();
+			psmt.close();
+			conn.close();
+
+		} catch (Exception e) {  
+			// TODO: handle exception  
+		}  
+
+		return rowCount;
+	}
 }
